648b91a459d53604f8b693ac5e51f3a693c597da
pkufool <njukingway@gmail.com>
2015-05-27 16:22:04 +0800

    add first activity, including the battery rate and mode listview

--- ApmPowerManager/app/src/main/java/hult/netlab/pku/apmpowermanager/MainActivity.java
+++ ApmPowerManager/app/src/main/java/hult/netlab/pku/apmpowermanager/MainActivity.java
@@ -1,17 +1,29 @@
 package hult.netlab.pku.apmpowermanager;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),batteryinfomain.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
